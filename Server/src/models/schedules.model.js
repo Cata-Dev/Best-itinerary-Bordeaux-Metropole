c7bbe51a09e2b1a10f7ffac34d769e97374d96a4
Catatomik <cata@catadev.ga>
2021-11-23 00:12:46 +0100

    :sparkles: Add incoming models for TBM API

---
+++ Server/src/models/schedules.model.js
@@ -0,0 +1,41 @@
+// schedules-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+	const modelName = 'schedules';
+	const mongooseClient = app.get('mongooseClient');
+	const { Schema } = mongooseClient;
+	const schema = new Schema({
+		gid: { type: Number, required: true, unique: true },
+		hor_theo: { type: Date, required: true },
+		hor_app: { type: Date, required: true },
+		hor_estime: { type: Date, required: true },
+		etat: { type: String, required: true },
+		type: { type: String, required: true }, //donnée incertaine
+		rs_sv_arret_p_id: { type: String, required: true },
+		rs_sv_cours_a_id: { type: String, required: true },
+	}, {
+		timestamps: true,
+		toObject: { virtuals: true },
+	});
+
+	schema.virtual('rs_sv_arret_p', {
+		ref: 'stops',
+		localField: 'rs_sv_arret_p_id',
+		foreignField: 'gid'
+	});
+	schema.virtual('rs_sv_cours_a', {
+		ref: 'vehicles',
+		localField: 'rs_sv_cours_a_id',
+		foreignField: 'gid'
+	});
+
+	// This is necessary to avoid model compilation errors in watch mode
+	// see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+	if (mongooseClient.modelNames().includes(modelName)) {
+		mongooseClient.deleteModel(modelName);
+	}
+	return mongooseClient.model(modelName, schema);
+  
+};
---
+++ Server/src/models/vehicles.model.js
@@ -0,0 +1,49 @@
+// vehicles-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+	const modelName = 'vehicles';
+	const mongooseClient = app.get('mongooseClient');
+	const { Schema } = mongooseClient;
+	const schema = new Schema({
+		gid: { type: Number, required: true, unique: true },
+		etat: { type: String, required: true },
+		rs_sv_ligne_a_id: { type: String, required: true },
+		rg_sv_arret_p_nd_id: { type: String, required: true },
+		rg_sv_arret_p_na_id: { type: String, required: true },
+		rg_sv_chem_l_id: { type: String, required: true },
+	}, {
+		timestamps: true,
+		toObject: { virtuals: true },
+	});
+
+	schema.virtual('rs_sv_ligne_a', {
+		ref: 'lines',
+		localField: 'rs_sv_ligne_a_id',
+		foreignField: 'gid'
+	});
+	schema.virtual('rg_sv_arret_p_nd', {
+		ref: 'stops',
+		localField: 'rg_sv_arret_p_nd_id',
+		foreignField: 'gid'
+	});
+	schema.virtual('rg_sv_arret_p_na', {
+		ref: 'stops',
+		localField: 'rg_sv_arret_p_na_id',
+		foreignField: 'gid'
+	});
+	schema.virtual('rg_sv_chem_l', {
+		ref: 'lines_routes',
+		localField: 'rg_sv_chem_l_id',
+		foreignField: 'gid'
+	});
+
+	// This is necessary to avoid model compilation errors in watch mode
+	// see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+	if (mongooseClient.modelNames().includes(modelName)) {
+		mongooseClient.deleteModel(modelName);
+	}
+	return mongooseClient.model(modelName, schema);
+  
+};
