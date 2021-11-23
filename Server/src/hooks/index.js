f9a3e74c43e52774caf5882992549c9c00991690
Catatomik <cata@catadev.ga>
2021-11-23 12:14:10 +0100

    :loud_sound:

--- Server/src/hooks/index.js
+++ Server/src/hooks/index.js
@@ -4,9 +4,9 @@ const { time, fR, fY, fB } = require('../utils/utils');
 module.exports = {
 
   log: (hookContext) => {
-    if (hookContext.params?.provider && hookContext.type == 'before') hookContext.params.beforeTs = performance.now();
-    else if (hookContext.params?.provider && hookContext.type == 'after') console.log(`${time.datetocompact(Date.now())} | (${(performance.now()-hookContext.params.beforeTs).toFixed(2)}ms) ${fB(hookContext.method.toUpperCase())} ${hookContext.path}${hookContext.id ? '/'+fY(hookContext.id) : ''} (provider: ${hookContext.params?.provider})`);
-    else if (hookContext.params?.provider && hookContext.type == 'error') console.log(`${time.datetocompact(Date.now())} | (${(performance.now()-hookContext.params.beforeTs).toFixed(2)}ms) ${fR(hookContext.method.toUpperCase())} ${hookContext.path}${hookContext.id ? '/'+fY(hookContext.id) : ''} (provider: ${hookContext.params?.provider})`);
+    if (hookContext.type == 'before') hookContext.params.beforeTs = performance.now();
+    else if (hookContext.type == 'after') console.log(`${time.datetocompact(Date.now())} | (${(performance.now()-hookContext.params.beforeTs).toFixed(2)}ms) ${fB(hookContext.method.toUpperCase())} ${hookContext.path}${hookContext.id ? '/'+fY(hookContext.id) : ''} (provider: ${hookContext.params?.provider || 'internal'})`);
+    else if (hookContext.type == 'error') console.log(`${time.datetocompact(Date.now())} | (${(performance.now()-hookContext.params.beforeTs).toFixed(2)}ms) ${fR(hookContext.method.toUpperCase())} ${hookContext.path}${hookContext.id ? '/'+fY(hookContext.id) : ''} (provider: ${hookContext.params?.provider || 'internal'})`);
     return hookContext;
   },
 
