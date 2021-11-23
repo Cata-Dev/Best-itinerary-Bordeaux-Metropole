3a993af4b75c335a0c205c30cb5de13d5afe4347
Catatomik <cata@catadev.ga>
2021-11-23 22:57:26 +0100

    :recycle:

--- Server/src/utils/TBM.js
+++ Server/src/utils/TBM.js
@@ -55,176 +55,183 @@ module.exports = (app) => {
 
 	return {
 
-		Intersections: async () => {
-			console.info(`Refreshing Intersections...`)
-			let intersections = await getData('fv_carre_p')
-			intersections = intersections.map(intersection => {
-				return new Intersection({
-					geo_point: intersection.geometry.coordinates,
-					gid: Number(intersection.properties.gid),
-					nature: intersection.properties.nature,
-				})
-			})
-			await Intersection.deleteMany({})
-			await Intersection.bulkSave(intersections)
-			console.info(`Intersections refreshed.`)
-			return true
-		},
+		endpoints: [
+			{
+				name: "Intersections", rate: 24*3600, fetching: false,
+				fetch: async () => {
+					console.info(`Refreshing Intersections...`)
+					let intersections = await getData('fv_carre_p')
+					intersections = intersections.map(intersection => {
+						return new Intersection({
+							geo_point: intersection.geometry.coordinates,
+							gid: Number(intersection.properties.gid),
+							nature: intersection.properties.nature,
+						})
+					})
+					await Intersection.deleteMany({})
+					await Intersection.bulkSave(intersections)
+					console.info(`Intersections refreshed.`)
+					return true
+				},
+				model: Intersection,
+			},
+			{
+				name: "Sections", rate: 24*3600, fetching: false,
+				fetch: async () => {
+					console.info(`Refreshing Sections...`)
+					let sections = await getData('fv_tronc_l')
+					sections = sections.map(section => {
+						return new Section({
+							distance: section.geometry.coordinates.reduce((acc, v, i, arr) => {
+								if (i < arr.length - 1) return acc+distance(...v, ...arr[i+1])
+								return acc
+							}, 0),
+							gid: Number(section.properties.gid),
+							domanial: Number(section.properties.domanial),
+							groupe: section.properties.groupe || 0,
+							nom_voie: section.properties.nom_voie,
+							rg_fv_graph_nd_id: section.properties.rg_fv_graph_nd,
+							rg_fv_graph_na_id: section.properties.rg_fv_graph_na,
+						})
+					})
+					await Section.deleteMany({})
+					await Section.bulkSave(sections)
+					console.info(`Sections refreshed.`)
+					return true
+				},
+				model: Section,
+			},
+			{
+				name: "Stops", rate: 24*3600, fetching: false,
+				fetch: async () => {
+					console.info(`Refreshing Stops...`)
+					let stops = await getData('sv_arret_p')
+					stops = stops.map(stop => {
+						return new Stop({
+							geo_point: stop.geometry?.coordinates || [NaN, NaN], //out of BM
+							gid: Number(stop.properties.gid),
+							libelle: stop.properties.libelle,
+							vehicule: stop.properties.vehicule,
+							type: stop.properties.type,
+							actif: stop.properties.actif,
+						})
+					})
+					await Stop.deleteMany({})
+					await Stop.bulkSave(stops)
+					console.info(`Stops refreshed.`)
+					return true
+				},
+				model: Stop,
+			},
+			{
+				name: "Lines", rate: 24*3600, fetching: false,
+				fetch: async () => {
+					console.info(`Refreshing Lines...`)
+					let lines = await getData('sv_ligne_a')
+					lines = lines.map(line => {
+						return new Line({
+							gid: Number(line.properties.gid),
+							libelle: line.properties.libelle,
+							vehicule: line.properties.vehicule,
+							active: line.properties.active,
+						})
+					})
+					await Line.deleteMany({})
+					await Line.bulkSave(lines)
+					console.info(`Lines refreshed.`)
+					return true
+				},
+				mode: Line,
+			},
+			{
+				name: "Schedules", rate: 10, fetching: false,
+				fetch: async () => {
+					console.info(`Refreshing Schedules...`)
+					const date = (new Date()).toJSON().substr(0, 19)
+					let schedules = await getData('sv_horai_a', ["filter="+JSON.stringify({
+						"$or": [
+							{
+								"hor_theo": {
+									"$gte": date
+								}
+							}, {
+								"hor_app": {
+									"$gte": date
+								},
+							}, {
+							"hor_estime": {
+									"$gte": date
+								},
+							}
+						]
+					})])
+					schedules = schedules.map(schedule => {
+						return new Schedule({
+							gid: Number(schedule.properties.gid),
+							hor_theo: new Date(schedule.properties.hor_theo),
+							hor_app: new Date(schedule.properties.hor_app),
+							hor_estime: new Date(schedule.properties.hor_estime),
+							etat: schedule.properties.etat,
+							type: schedule.properties.type,
+							rs_sv_arret_p_id: Number(schedule.properties.rs_sv_arret_p),
+							rs_sv_cours_a_id: Number(schedule.properties.rs_sv_cours_a),
+						})
+					})
+					await Schedule.deleteMany({})
+					await Schedule.bulkSave(schedules)
+					console.info(`Schedules refreshed.`)
+					return true
+				},
+				model: Schedule,
+			},
+			{
+				name: "Vehicles", rate: 10*60, fetching: false,
+				fetch: async () => {
+					console.info(`Refreshing Vehicles...`)
+					let vehicles = await getData('sv_cours_a')
+					vehicles = vehicles.map(vehicle => {
+						return new Vehicle({
+							gid: Number(vehicle.properties.gid),
+							etat: vehicle.properties.etat,
+							rg_sv_arret_p_nd_id: Number(vehicle.properties.rg_sv_arret_p_nd),
+							rg_sv_arret_p_na_id: Number(vehicle.properties.rg_sv_arret_p_na),
+							rs_sv_ligne_a_id: Number(vehicle.properties.rs_sv_ligne_a),
+							rg_sv_chem_l_id: Number(vehicle.properties.rs_sv_chem_l),
+						})
+					})
+					await Vehicle.deleteMany({})
+					await Vehicle.bulkSave(vehicles)
+					console.info(`Vehicles refreshed.`)
+					return true
+				},
+				model: Vehicle,
+			},
+			{
+				name: "Lines_routes", rate: 3600, fetching: false,
+				fetch: async () => {
+					console.info(`Refreshing Lines_routes...`)
+					let lines_routes = await getData('sv_chem_l', ["attributes="+JSON.stringify([
+						"gid", "libelle", "sens", "vehicule", "rs_sv_ligne_a", "rg_sv_arret_p_nd", "rg_sv_arret_p_na"
+					])])
+					lines_routes = lines_routes.map(lines_route => {
+						return new Lines_route({
+							gid: Number(lines_route.properties.gid),
+							libelle: lines_route.properties.libelle,
+							sens: lines_route.properties.sens,
+							vehicule: lines_route.properties.vehicule,
+							rs_sv_ligne_a_id: Number(lines_route.properties.rs_sv_ligne_a),
+							rg_sv_arret_p_nd_id: Number(lines_route.properties.rg_sv_arret_p_nd),
+							rg_sv_arret_p_na_id: Number(lines_route.properties.rg_sv_arret_p_na),
+						})
+					})
+					await Lines_route.deleteMany({})
+					await Lines_route.bulkSave(lines_routes)
+					console.info(`Lines_routes refreshed.`)
+					return true
+				},
+				model: Lines_route,
+			},
+		]
 
-		Sections: async () => {
-			console.info(`Refreshing Sections...`)
-			let sections = await getData('fv_tronc_l')
-			sections = sections.map(section => {
-				return new Section({
-					distance: section.geometry.coordinates.reduce((acc, v, i, arr) => {
-						if (i < arr.length - 1) return acc+distance(...v, ...arr[i+1])
-						return acc
-					}, 0),
-					gid: Number(section.properties.gid),
-					domanial: Number(section.properties.domanial),
-					groupe: section.properties.groupe || 0,
-					nom_voie: section.properties.nom_voie,
-					rg_fv_graph_nd_id: section.properties.rg_fv_graph_nd,
-					rg_fv_graph_na_id: section.properties.rg_fv_graph_na,
-				})
-			})
-			await Section.deleteMany({})
-			await Section.bulkSave(sections)
-			console.info(`Sections refreshed.`)
-			return true
-		},
-
-		Stops: async () => {
-			console.info(`Refreshing Stops...`)
-			let stops = await getData('sv_arret_p')
-			stops = stops.map(stop => {
-				return new Stop({
-					geo_point: stop.geometry?.coordinates || [NaN, NaN], //out of BM
-					gid: Number(stop.properties.gid),
-					libelle: stop.properties.libelle,
-					vehicule: stop.properties.vehicule,
-					type: stop.properties.type,
-					actif: stop.properties.actif,
-				})
-			})
-			await Stop.deleteMany({})
-			await Stop.bulkSave(stops)
-			console.info(`Stops refreshed.`)
-			return true
-		},
-
-		Lines: async () => {
-			console.info(`Refreshing Lines...`)
-			let lines = await getData('sv_ligne_a')
-			lines = lines.map(line => {
-				return new Line({
-					gid: Number(line.properties.gid),
-					libelle: line.properties.libelle,
-					vehicule: line.properties.vehicule,
-					active: line.properties.active,
-				})
-			})
-			await Line.deleteMany({})
-			await Line.bulkSave(lines)
-			console.info(`Lines refreshed.`)
-			return true
-		},
-
-		Schedules: async () => {
-			console.info(`Refreshing Schedules...`)
-			const date = (new Date()).toJSON().substr(0, 19)
-			let schedules = await getData('sv_horai_a', ["filter="+JSON.stringify({
-				"$or": [
-					{
-						"hor_theo": {
-							"$gte": date
-						}
-					}, {
-						"hor_app": {
-							"$gte": date
-						},
-					}, {
-					"hor_estime": {
-							"$gte": date
-						},
-					}
-				]
-			})])
-			schedules = schedules.map(schedule => {
-				return new Schedule({
-					gid: Number(schedule.properties.gid),
-					hor_theo: new Date(schedule.properties.hor_theo),
-					hor_app: new Date(schedule.properties.hor_app),
-					hor_estime: new Date(schedule.properties.hor_estime),
-					etat: schedule.properties.etat,
-					type: schedule.properties.type,
-					rs_sv_arret_p_id: Number(schedule.properties.rs_sv_arret_p),
-					rs_sv_cours_a_id: Number(schedule.properties.rs_sv_cours_a),
-				})
-			})
-			await Schedule.deleteMany({})
-			await Schedule.bulkSave(schedules)
-			console.info(`Schedules refreshed.`)
-			return true
-		},
-
-		Vehicles: async () => {
-			console.info(`Refreshing Vehicles...`)
-			let vehicles = await getData('sv_cours_a')
-			vehicles = vehicles.map(vehicle => {
-				return new Vehicle({
-					gid: Number(vehicle.properties.gid),
-					etat: vehicle.properties.etat,
-					rg_sv_arret_p_nd_id: Number(vehicle.properties.rg_sv_arret_p_nd),
-					rg_sv_arret_p_na_id: Number(vehicle.properties.rg_sv_arret_p_na),
-					rs_sv_ligne_a_id: Number(vehicle.properties.rs_sv_ligne_a),
-					rg_sv_chem_l_id: Number(vehicle.properties.rs_sv_chem_l),
-				})
-			})
-			await Vehicle.deleteMany({})
-			await Vehicle.bulkSave(vehicles)
-			console.info(`Vehicles refreshed.`)
-			return true
-		},
-
-		Lines_routes: async () => {
-			console.info(`Refreshing Lines_routes...`)
-			let lines_routes = await getData('sv_chem_l')
-			lines_routes = lines_routes.map(lines_route => {
-				return new Lines_route({
-					gid: Number(lines_route.properties.gid),
-					libelle: lines_route.properties.libelle,
-					sens: lines_route.properties.sens,
-					vehicule: lines_route.properties.vehicule,
-					rs_sv_ligne_a_id: Number(lines_route.properties.rs_sv_ligne_a),
-					rg_sv_arret_p_nd_id: Number(lines_route.properties.rg_sv_arret_p_nd),
-					rg_sv_arret_p_na_id: Number(lines_route.properties.rg_sv_arret_p_na),
-				})
-			})
-			await Lines_route.deleteMany({})
-			await Lines_route.bulkSave(lines_routes)
-			console.info(`Lines_routes refreshed.`)
-			return true
-		},
-
-		Models: {
-			Intersection,
-			Section,
-			Stop,
-			Line,
-			Schedule,
-			Vehicle,
-			Lines_route,
-		},
-
-		endpoints: { //every API endpoints we can update + their actualization rate (in seconds)
-			Intersections: 24*3600,
-			Sections: 24*3600,
-			Stops: 24*3600,
-			Lines: 24*3600,
-			Schedules: 10,
-			Vehicles: 10*60,
-			Lines_routes: 3600,
-		}
 	}
 }
