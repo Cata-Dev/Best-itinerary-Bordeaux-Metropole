a31be25676293bf6bf619a2d69f09488b2d6b986
Catatomik <cata@catadev.ga>
2021-12-11 20:12:58 +0100

    💄

--- Client/src/components/Footer.vue
+++ Client/src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid grid-cols-3 gap-3 justify-center content-center">
     <div></div>
-    <p class="my-auto text-center text-t-light-faded dark:text-t-dark-faded">Trouver le meilleur itinéraire sur Bordeaux Métropole © 2021</p>
-    <button class="justify-self-end my-2 mr-4" @click="toggleDarkMode">
-        <font-awesome-icon :icon="theme === 'light' ? 'moon' : 'sun'" class="align-self-center " style="font-size: 24px;"/>
+    <p class="my-auto text-center text-sm text-t-light-faded dark:text-t-dark-faded">Trouver le meilleur itinéraire sur Bordeaux Métropole © 2021</p>
+    <button class="justify-self-end self-center my-1 mr-4" @click="toggleDarkMode">
+        <font-awesome-icon :icon="theme === 'light' ? 'moon' : 'sun'" class="" style="font-size: 20px;"/>
     </button>
   </div>
 </template>
