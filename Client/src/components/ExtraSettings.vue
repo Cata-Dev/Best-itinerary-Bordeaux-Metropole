2f49a06c845a8c2713f8dc58f4be12e5574b6774
Catatomik <cata@catadev.ga>
2021-12-12 20:30:05 +0100

    🚧 Configure itinerary

---
+++ Client/src/components/ExtraSettings.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ExtraSettings',
+    props: {
+        modelValue: String,
+    },
+    emits: [
+        'update:modelValue',
+    ],
+    setup() {
+        
+        return {
+            
+        }
+    },
+}
+</script>
