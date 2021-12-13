6e1c6d3f89bf90897efad9e6385f9ee18bebaf3a
Catatomik <cata@catadev.ga>
2021-12-13 16:02:46 +0100

    🚧 Add faSlidersH

--- Client/src/main.js
+++ Client/src/main.js
@@ -2,16 +2,16 @@ import { createApp } from 'vue'
 import { router } from './router';
 import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCrosshairs, faSearchLocation, faMoon, faSun, faMapPin, faFlag } from '@fortawesome/free-solid-svg-icons'
+import { faCrosshairs, faSearchLocation, faMoon, faSun, faMapPin, faFlag, faSlidersH } from '@fortawesome/free-solid-svg-icons'
 import {  } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCrosshairs, faSearchLocation, faMoon, faSun, faMapPin, faFlag )
+library.add(faCrosshairs, faSearchLocation, faMoon, faSun, faMapPin, faFlag, faSlidersH )
 import App from './App.vue'
 
 const app = createApp(App)
 app
-    .component('font-awesome-icon', FontAwesomeIcon)
+    .component('FontAwesomeIcon', FontAwesomeIcon)
 app
     .use(router)
     .use(router)
