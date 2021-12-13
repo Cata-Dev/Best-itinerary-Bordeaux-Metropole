7390ef08ddf9cd15e4dda55052fb9efd3ec0031e
Catatomik <cata@catadev.ga>
2021-12-13 10:38:32 +0100

    🏗️ Fix logical LocationSearch behavior

--- Client/src/components/LocationSearch.vue
+++ Client/src/components/LocationSearch.vue
@@ -3,32 +3,102 @@
         <button class="flex mr-1 items-center">
             <font-awesome-icon icon="crosshairs" class="text-t-light-primary dark:text-t-dark-primary" style="font-size: 22px;"/>
         </button>
-        <input type="text" :list="name" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" class="flex-grow border-none focus:ring-transparent bg-bg-light dark:bg-bg-dark text-t-light-primary dark:text-t-dark-primary placeholder-t-light-faded dark:placeholder-t-dark-faded" :placeholder="placeholder">
+        <input type="text" :list="name" v-model="location.input" @keyup="refreshSuggestions()"
+            class="
+				flex-grow
+				border-none
+				focus:ring-transparent
+				bg-bg-light
+				dark:bg-bg-dark
+				text-t-light-primary
+				dark:text-t-dark-primary
+				placeholder-t-light-faded
+				dark:placeholder-t-dark-faded"
+            :placeholder="placeholder">
         <datalist :id="name">
-            <option v-for="data in datalist" :key="data.value" :value="data.display">{{ data.type }}</option>
+            <option v-for="data in location.datalist" :key="data.value" :value="data.display">{{ data.type }}</option>
         </datalist>
         <span class="flex mr-1 items-center">
-            <font-awesome-icon :icon="name == 'destination' ? 'flag' : 'map-pin'" class="text-t-light-primary dark:text-t-dark-primary ml-1" style="font-size: 22px;"/>
+            <font-awesome-icon :icon="name == 'destination' ? 'flag' : 'map-pin'"
+                class="
+                    text-t-light-primary
+                    dark:text-t-dark-primary
+                    ml-1"
+                style="font-size: 22px;"/>
         </span>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { client } from '../store/'
+
 export default {
     name: 'LocationSearch',
     props: {
         name: String,
-        datalist: Array,
         placeholder: String,
         modelValue: String,
     },
     emits: [
         'update:modelValue',
     ],
-    setup() {
+    setup(props, ctx) {
+
+        const location = ref({
+            datalist: [],
+            input: '',
+            previousInput: null,
+            updated: Date.now(),
+        })
+
+        function updateModelValue(v) {
+            ctx.emit('update:modelValue', JSON.stringify(v))
+        }
+
+        function parseGeocode(s) {
+
+            switch(s.GEOCODE_type) {
+                case 'Addresses':
+                    return { display: `${s.numero} ${s.rep ? s.rep+' ' : ''}${s.nom_voie} ${s.commune}`, type: "Adresse" }
+
+                case 'TBM_Stops':
+                    return { display: s.libelle, type: s.vehicule }
+
+                case 'SNCF_Stops':
+                    return { display: s.name, type: "Train" }
+
+                default: return null
+
+            }
+        }
+
+        async function refreshSuggestions() {
+
+            if (location.value.previousInput === location.value.input) return
+            if (location.value.input.length < 5) return location.value.datalist = []
+            const validLocation = location.value.datalist.find(l => l.display == location.value.input)
+            if (validLocation) {
+                updateModelValue(validLocation)
+                return
+            }
+            const now = Date.now()
+            let suggestions
+            try {
+                suggestions = await client.service('geocode').find({ query: { id: location.value.input, max: 25, uniqueVoies: true } })
+            // eslint-disable-next-line no-empty
+            } catch(_) {}
+            if (now < location.value.updated) return
+            location.value.datalist = suggestions && suggestions.length ? suggestions.map(s => ({ value: s.coords, ...parseGeocode(s) })) : []
+            location.value.previousInput = location.value.input
+            location.value.updated = now
+
+        }
         
         return {
-            
+            location,
+            updateModelValue,
+            refreshSuggestions,
         }
     },
 }
--- Client/src/views/Home.vue
+++ Client/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex flex-col flex-wrap max-h-full h-4/5 w-full justify-center content-center">
-        <LocationSearch name="source" ref="sourceElem" :datalist="source.datalist" @keyup="refreshSuggestions(source)" v-model="source.input" placeholder="Départ" class="my-1" />
-        <LocationSearch name="destination" ref="destinationElem" :datalist="destination.datalist" @keyup="refreshSuggestions(destination)" v-model="destination.input" placeholder="Arrivée" class="my-1" />
-        <ExtraSettings v-model="o"/>
+        <LocationSearch name="source" ref="sourceElem" v-model="source" placeholder="Départ"/>
+        <LocationSearch name="destination" ref="destinationElem" v-model="destination" placeholder="Arrivée" class="mt-2" />
+        <ExtraSettings v-model="settings" class="mt-2"/>
     </div>
 </template>
 
@@ -10,7 +10,7 @@
 import { ref } from 'vue'
 import LocationSearch from '../components/LocationSearch.vue'
 import ExtraSettings from '../components/ExtraSettings.vue'
-import { client } from '../store/'
+// import { client } from '../store/'
 
 export default {
     name: 'Home',
@@ -20,59 +20,14 @@ export default {
     },
     setup() {
 
-        const source = ref({
-            datalist: [],
-            input: '',
-            previousInput: null,
-            updated: Date.now(),
-        })
-        const destination = ref({
-            datalist: [],
-            input: '',
-            previousInput: null,
-            updated: Date.now(),
-        })
-
-        function parseGeocode(s) {
-
-            switch(s.GEOCODE_type) {
-
-                case 'Addresses':
-                    return { display: `${s.numero} ${s.rep ? s.rep+' ' : ''}${s.nom_voie} ${s.commune}`, type: "Adresse" }
-
-                case 'TBM_Stops':
-                    return { display: s.libelle, type: s.vehicule }
-
-                case 'SNCF_Stops':
-                    return { display: s.name, type: "Train" }
-
-                default: return null
-
-            }
-
-        }
-
-        async function refreshSuggestions(type) {
-
-            if (type.previousInput === type.input) return
-            if (type.input.length < 5) return type.datalist = []
-            const now = Date.now()
-            let suggestions
-            try {
-                suggestions = await client.service('geocode').find({ query: { id: type.input, max: 25, uniqueVoies: true } })
-            // eslint-disable-next-line no-empty
-            } catch(_) {}
-            if (now < type.updated) return
-            type.datalist = suggestions && suggestions.length ? suggestions.map(s => ({ value: s.coords, ...parseGeocode(s) })) : []
-            type.previousInput = type.input
-            type.updated = now
-
-        }
+        const source = ref(null)
+        const destination = ref(null)
+        const settings = ref(null)
 
         return {
             source,
             destination,
-            refreshSuggestions,
+            settings,
         }
     },
 }
