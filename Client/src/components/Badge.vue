38054e67f12db38ff6f786c3a4a76e95bc0c745e
Catatomik <cata@catadev.ga>
2021-12-16 16:01:13 +0100

    ✨ Add DynamicBadge component, a bit customizable

---
+++ Client/src/components/Badge.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    class="
+        flex
+        my-[0.33rem]
+        ml-4
+        py-[0.125rem]
+        px-2
+        rounded-full
+        items-center
+        border-2"
+    :class="badegStyle"
+  >
+    <span 
+      class="mr-1"
+      :class="color === 'success' ? ['text-success-t'] : color === 'info' ? ['text-info-t'] : ['text-alert-t']"
+    >
+      {{ text }}
+    </span>
+    <div
+      v-if="icon === 'loading'"
+      class="
+        spinner-border
+        animate-spin
+        inline-block
+        w-5
+        h-5
+        squared
+        border-2
+        rounded-full
+        border-info-t"
+      role="status"
+    />
+    <font-awesome-icon
+      v-else
+      :icon="icon"
+      class="text-xl"
+      :class="`text-${color}-t`"
+    />
+  </div>
+</template>
+
+<script>
+import { ref, onBeforeUpdate } from 'vue'
+
+export default {
+    name: 'DynamicBadge',
+    props: {
+        text: {
+            type: String,
+            required: true,
+            default: '',
+        },
+        icon: {
+            type: String,
+            required: true,
+            default: 'info',
+        },
+        color: {
+            type: String,
+            required: true,
+            default: '',
+        },
+        bg: {
+            type: Boolean,
+            required: false,
+            default: true
+        },
+    },
+    setup(props) {
+
+        let badegStyle = ref({
+            [`bg-${props.color}-bg`]: props.bg,
+            [`border-${props.color}-t`]: true,
+        })
+
+        onBeforeUpdate(() => {
+            for (const c in badegStyle.value) {
+                delete badegStyle.value[c]
+            }
+            badegStyle.value[`bg-${props.color}-bg`] = props.bg
+            badegStyle.value[`border-${props.color}-t`] = true
+        })
+
+      return {
+          badegStyle
+      }
+    },
+}
+</script>
+
+<style>
+.spinner-border {
+    border-right: .25em solid transparent;
+}
+</style>
