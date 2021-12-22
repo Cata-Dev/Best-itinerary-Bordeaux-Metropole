188ab4a573fffb0e34762a001165707633443067
Catatomik <cata@catadev.ga>
2021-12-22 20:23:35 +0100

    🚧 Add required fa icons

--- Client/src/main.js
+++ Client/src/main.js
@@ -2,11 +2,35 @@ import { createApp } from 'vue'
 import { router } from './router';
 import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCrosshairs, faSearchLocation, faMoon, faSun, faMapPin, faFlag, faSlidersH, faCheckCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import {
+    faCrosshairs,
+    faSearchLocation,
+    faMoon,
+    faSun,
+    faMapPin,
+    faFlag,
+    faSlidersH,
+    faCheckCircle,
+    faExclamationTriangle,
+    faTimes,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons'
 import {  } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCrosshairs, faSearchLocation, faMoon, faSun, faMapPin, faFlag, faSlidersH, faCheckCircle, faExclamationTriangle )
+library.add(
+    faCrosshairs,
+    faSearchLocation,
+    faMoon,
+    faSun,
+    faMapPin,
+    faFlag,
+    faSlidersH,
+    faCheckCircle,
+    faExclamationTriangle,
+    faTimes,
+    faSpinner,
+)
 import App from './App.vue'
 
 const app = createApp(App)
