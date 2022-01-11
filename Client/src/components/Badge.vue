6cccf6e39286091abf5eacb515c301555f15b44f
Catatomik <cata@catadev.ga>
2022-01-11 16:27:14 +0100

    💄 Fix design

--- Client/src/components/Badge.vue
+++ Client/src/components/Badge.vue
@@ -24,18 +24,16 @@ defineProps({
 <template>
   <div
     class="
-        flex
-        my-[0.33rem]
-        ml-4
-        py-[0.125rem]
-        px-2
-        rounded-full
-        items-center
-        border-2"
+      flex
+      py-[0.125rem]
+      px-2
+      rounded-full
+      items-center
+      border-2"
     :class="color === 'success' ? [{'bg-success-bg': bg}, 'border-success-t'] : color === 'info' ? [{'bg-info-bg': bg}, 'border-info-t'] : [{'bg-alert-bg': bg}, 'border-alert-t']"
   >
     <span 
-      class="mr-1 text-sm"
+      class="mr-1 text-sm whitespace-nowrap"
       :class="color === 'success' ? ['text-success-t'] : color === 'info' ? ['text-info-t'] : ['text-alert-t']"
     >
       {{ text }}
