c14bd29a3d891133f40b6e13eb30b3a65f79f359
Catatomik <cata@catadev.ga>
2022-01-11 16:30:27 +0100

    💄 Enhanced design

--- Client/src/components/ExtraSettings.vue
+++ Client/src/components/ExtraSettings.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import { theme } from '../store'
 
 const props = defineProps({
   shown: {
@@ -20,7 +21,7 @@ defineEmits([
 </script>
 
 <template>
-  <div class="flex items-center">
+  <div>
     <div
       ref="accordion"
       class="
@@ -41,53 +42,62 @@ defineEmits([
       :class="{ 'max-w-full': shown }"
     >
       <div class="flex flex-col m-2">
-        <div class="inline-block">
-          <span>Heure de départ :</span>
-          <input
-            v-model="settings.departureTime"
-            type="time"
-            class="bg-transparent text-inherit time mx-1"
-            @input="$emit('update:modelValue', settings)"
-          >
+        <div class="inline-block mb-1">
+          <span class="mr-2">Heure de départ</span>
+          <div class="inline-block p-1 px-2 bg-bg-light-contrasted dark:bg-bg-dark-contrasted rounded-md">
+            <input
+              v-model="settings.departureTime"
+              type="time"
+              class="bg-transparent text-inherit time"
+              :class="{ 'dark': theme === 'dark' }"
+              @input="$emit('update:modelValue', settings)"
+            >
+          </div>
+        </div>
+        <div class="inline-block mb-1">
+          <span class="mr-2">Distance max à pied</span>
+          <div class="inline-block p-1 px-2 bg-bg-light-contrasted dark:bg-bg-dark-contrasted rounded-md">
+            <input
+              v-model="settings.maxWalkDistance"
+              class="w-16"
+              step="10"
+              type="number"
+              @change="$emit('update:modelValue', settings)"
+            >
+            <span>m</span>
+          </div>
+        </div>
+        <div class="inline-block mb-1">
+          <span class="mr-2">Vitesse de marche</span>
+          <div class="inline-block p-1 px-2 bg-bg-light-contrasted dark:bg-bg-dark-contrasted rounded-md">
+            <input
+              v-model="settings.walkSpeed"
+              class="w-12"
+              step="0.1"
+              type="number"
+              @input="$emit('update:modelValue', settings)"
+            >
+            <span>km/h</span>
+          </div>
         </div>
         <div class="inline-block">
-          <span class="mr-1">Distance max à pied :</span>
-          <input
-            v-model="settings.maxWalkDistance"
-            class="w-24"
-            step="10"
-            type="number"
-            @change="$emit('update:modelValue', settings)"
-          >
-          <span>m</span>
-        </div>
-        <div class="inline-block">
-          <span class="mr-1">Vitesse de marche :</span>
-          <input
-            v-model="settings.walkSpeed"
-            class="w-12"
-            step="0.1"
-            type="number"
-            @input="$emit('update:modelValue', settings)"
-          >
-          <span>km/h</span>
-        </div>
-        <div class="inline-block">
-          <span class="">Modes de transport :</span>
-          <br><input
-            v-model="settings.transports.TBM"
-            class="ml-2"
-            checked="true"
-            type="checkbox"
-            @change="$emit('update:modelValue', settings)"
-          ><span class="ml-1">TBM</span>
-          <br><input
-            v-model="settings.transports.SNCF"
-            class="ml-2"
-            checked="true"
-            type="checkbox"
-            @change="$emit('update:modelValue', settings)"
-          ><span class="ml-1">SNCF</span>
+          <span class="">Modes de transport</span>
+          <div class="w-fit p-1 px-2 bg-bg-light-contrasted dark:bg-bg-dark-contrasted rounded-md">
+            <input
+              v-model="settings.transports.TBM"
+              class="ml-2"
+              checked="true"
+              type="checkbox"
+              @change="$emit('update:modelValue', settings)"
+            ><span class="ml-1">TBM</span>
+            <br><input
+              v-model="settings.transports.SNCF"
+              class="ml-2"
+              checked="true"
+              type="checkbox"
+              @change="$emit('update:modelValue', settings)"
+            ><span class="ml-1">SNCF</span>
+          </div>
         </div>
       </div>
     </div>
@@ -95,7 +105,10 @@ defineEmits([
 </template>
 
 <style scoped>
-input[type="time"]::-webkit-calendar-picker-indicator {
-  filter: invert(100%)
+.time.dark::-webkit-calendar-picker-indicator {
+  filter: invert(90%)
+}
+.time::-webkit-datetime-edit {
+  max-width: 5ch;
 }
 </style>
