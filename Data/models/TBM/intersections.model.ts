550d7686ee240bbd07878deea9f12e33b431598f
Catatomik <cata@catadev.org>
2024-02-07 19:54:52 +0100

    ✨ Centralize data models

---
+++ Data/models/TBM/intersections.model.ts
@@ -0,0 +1,35 @@
+// intersections-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+
+import { TBMEndpoints } from "./names";
+import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { addModelToTypegoose, buildSchema, deleteModelWithClass, getModelForClass, prop } from "@typegoose/typegoose";
+import { modelOptions } from "@typegoose/typegoose/lib/modelOptions";
+import { getName } from "@typegoose/typegoose/lib/internal/utils";
+import { Connection } from "mongoose";
+
+@modelOptions({ options: { customName: TBMEndpoints.Intersections } })
+export class dbIntersections extends TimeStamps {
+  @prop({ required: true })
+  public _id!: number;
+
+  @prop({ type: () => [Number, Number], required: true })
+  public coords!: [number, number];
+
+  @prop({ required: true })
+  public nature!: string;
+}
+
+export default function init(db: Connection) {
+  if (getModelForClass(dbIntersections, { existingConnection: db })) deleteModelWithClass(dbIntersections);
+
+  const dbIntersectionsSchema = buildSchema(dbIntersections, { existingConnection: db });
+  const dbIntersectionsModelRaw = db.model(getName(dbIntersections), dbIntersectionsSchema);
+
+  return addModelToTypegoose(dbIntersectionsModelRaw, dbIntersections, {
+    existingConnection: db,
+  });
+}
+
+export type dbIntersectionsModel = ReturnType<typeof init>;
---
+++ Data/models/TBM/names.ts
@@ -0,0 +1,13 @@
+export enum TBMEndpoints {
+  Addresses = "Addresses",
+  Intersections = "Intersections",
+  Sections = "Sections",
+  Stops = "TBM_Stops",
+  Lines = "TBM_Lines",
+  // 2 different endpoints in 1 collection
+  Schedules = "TBM_Schedules",
+  Schedules_rt = "TBM_Schedules_rt",
+  Trips = "TBM_Trips",
+  Lines_routes = "TBM_Lines_routes",
+  ScheduledRoutes = "TBM_Scheduled_routes",
+}
