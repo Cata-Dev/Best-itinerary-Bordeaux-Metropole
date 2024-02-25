91b463b537046d5b20ddaf1f025255c41414757f
Catatomik <cata@catadev.org>
2024-02-25 22:48:52 +0100

    ⬆️ eslint flat configs

---
+++ Server/eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  // @ts-ignore
+  files: ["**/*.ts"],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+  ],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      project: true,
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { caughtErrorsIgnorePattern: "^_+$", argsIgnorePattern: "^_+$", varsIgnorePattern: "^_+$" },
+    ],
+    indent: "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-empty-interface": ["warn"],
+    "no-empty": [
+      "warn",
+      {
+        allowEmptyCatch: true,
+      },
+    ],
+  },
+});
