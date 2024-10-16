812eac4b259f807ca4c27ce07659da15d4cfb9fa
Catatomik <cata@catadev.org>
2024-10-16 23:25:48 +0200

    🚧 Directly call refresh-data service

--- Client/src/store/feathers/feathers.ts
+++ Client/src/store/feathers/feathers.ts
@@ -1,9 +1,8 @@
 import io from "socket.io-client";
 import socketio from "@feathersjs/socketio-client";
-import type { TBMEndpoints } from "server/externalAPIs/TBM/index";
 import { createClient } from "server";
-import type { Itinerary, ItineraryQuery } from "server";
-import type { TransportMode } from "../";
+import type { RefreshData } from "server";
+import { Deferred } from "common/async";
 
 const connection = socketio(
   io("https://bibm.catadev.org", {
@@ -14,46 +13,14 @@ const connection = socketio(
 );
 const client = createClient(connection);
 
-interface PromisedAPI {
-  result: Promise<Itinerary>;
-  resolve: (value: Itinerary) => void;
-  reject: (reason?: { code?: number }) => void;
-}
-
-const APIRefresh: PromisedAPI = {} as PromisedAPI;
-
-APIRefresh.result = new Promise((resolve, reject) => {
-  APIRefresh.resolve = resolve;
-  APIRefresh.reject = reject;
-});
-
-interface ItineraryQueryLocationOverride {
-  type: Exclude<TransportMode, "FOOT"> | TBMEndpoints.Addresses;
-}
-
-type Location = Omit<ItineraryQuery["from"], keyof ItineraryQueryLocationOverride> &
-  ItineraryQueryLocationOverride;
-
-const defaultLocation = {
-  id: -1,
-  type: "Addresses" as TBMEndpoints.Addresses,
-  coords: [-1, -1] satisfies [unknown, unknown],
-  alias: "unknown",
-} satisfies Location;
+const APIRefresh = new Deferred<RefreshData>();
 
 client
-  .service("itinerary")
-  .get("paths", {
-    query: { waitForUpdate: true, from: defaultLocation, to: defaultLocation },
+  .service("refresh-data")
+  .get("all", {
+    query: { waitForUpdate: true },
   })
-  .then((r) => {
-    if (r.code && r.code == 200) {
-      APIRefresh.resolve(r);
-    } else APIRefresh.reject(r);
-  })
-  .catch((e) => {
-    APIRefresh.reject(e);
-  });
+  .then(APIRefresh.resolve)
+  .catch(APIRefresh.reject);
 
-export { client, APIRefresh, defaultLocation };
-export type { Location };
+export { client, APIRefresh };
