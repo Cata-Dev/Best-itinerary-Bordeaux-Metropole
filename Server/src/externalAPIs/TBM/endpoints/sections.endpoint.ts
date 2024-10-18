00fe4867ce09e5ed0b1aeb5cec0ed9c04d531d89
Catatomik <cata@catadev.org>
2024-10-18 19:18:37 +0200

    🐛 Section coordinates can be MultiLineString

--- Server/src/externalAPIs/TBM/endpoints/sections.endpoint.ts
+++ Server/src/externalAPIs/TBM/endpoints/sections.endpoint.ts
@@ -32,16 +32,22 @@ export default (app: Application, getData: <T>(id: string, queries?: string[]) =
         const rawSections: Section[] = await getData("fv_tronc_l", ["crs=epsg:2154"]);
 
         const Sections: dbSections[] = rawSections.map((section) => {
+          if ((section.geometry.coordinates?.[0]?.[0] as never)?.[0] !== undefined)
+            // Depth 3 => MultiLineString
+            section.geometry.coordinates =
+              section.geometry.coordinates.flat() as unknown as Section["geometry"]["coordinates"];
+
           return {
             coords: section.geometry.coordinates,
             distance: section.geometry.coordinates.reduce((acc: number, v, i, arr) => {
               if (i < arr.length - 1) return acc + euclideanDistance(...v, ...arr[i + 1]);
+
               return acc;
             }, 0),
             _id: parseInt(section.properties.gid),
-            domanial: parseInt(section.properties.domanial) || 0,
-            cat_dig: parseInt(section.properties.cat_dig),
-            groupe: section.properties.groupe || 0,
+            domanial: parseInt(section.properties.domanial ?? 7),
+            cat_dig: parseInt(section.properties.cat_dig ?? 10),
+            groupe: section.properties.groupe ?? 0,
             nom_voie: section.properties.nom_voie,
             rg_fv_graph_dbl: !!section.properties.rg_fv_graph_dbl,
             rg_fv_graph_nd: section.properties.rg_fv_graph_nd,
