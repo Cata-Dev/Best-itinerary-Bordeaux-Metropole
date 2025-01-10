0ac24fc94712717488ed81b1b30eb437345a568c
Catatomik <cata@catadev.org>
2025-01-10 22:42:34 +0100

    ✨ Simple cache

--- Common/src/cache.ts
+++ Common/src/cache.ts
@@ -1,3 +1,38 @@
+/**
+ * Simple cache for a single data without TTL, like an updating wrapper
+ */
+class Cache<D, I> {
+  private _lastUpdate = -1;
+
+  constructor(
+    private data: D,
+    private internal: I,
+    private readonly freshness: (internal: I) => number | Promise<number>,
+    private readonly update: (internal: I) => D | Promise<D>,
+  ) {}
+
+  async get() {
+    let updated = false;
+
+    if ((await this.freshness(this.internal)) > this._lastUpdate) {
+      this.data = await this.update(this.internal);
+      this._lastUpdate = Date.now();
+      updated = true;
+    }
+
+    return [
+      updated,
+      this._lastUpdate,
+      /** Do NOT edit this one or it will make side-effects */
+      this.data,
+    ] as const;
+  }
+
+  get lastUpdate() {
+    return this._lastUpdate;
+  }
+}
+
 function memoize<A extends unknown[], R>(f: (...args: A) => R) {
   const cache = new Map<string, R>();
 
@@ -12,4 +47,4 @@ function memoize<A extends unknown[], R>(f: (...args: A) => R) {
   };
 }
 
-export { memoize };
+export { Cache, memoize };
