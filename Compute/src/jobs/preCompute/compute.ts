01bb8eef8d2d50199c4162f5bce48ff3b08b4fe0
Catatomik <cata@catadev.org>
2025-01-19 02:02:42 +0100

    🎨

--- Compute/src/jobs/preCompute/compute.ts
+++ Compute/src/jobs/preCompute/compute.ts
@@ -5,7 +5,7 @@ import { DocumentType } from "@typegoose/typegoose";
 import { mapAsync } from "common/async";
 import { Logger } from "common/logger";
 import NonScheduledRoutesModelInit, { dbFootPaths } from "data/models/TBM/NonScheduledRoutes.model";
-import TBMSchedulesInit from "data/models/TBM/TBM_schedules.model";
+import TBMSchedulesInit, { dbTBM_Schedules_rt } from "data/models/TBM/TBM_schedules.model";
 import stopsModelInit, { dbTBM_Stops } from "data/models/TBM/TBM_stops.model";
 import TBMScheduledRoutesModelInit, { dbTBM_ScheduledRoutes } from "data/models/TBM/TBMScheduledRoutes.model";
 import { FilterQuery } from "mongoose";
@@ -14,8 +14,47 @@ import { parentPort } from "node:worker_threads";
 import { SharedRAPTORData } from "raptor/lib/SharedStructures";
 import { preComputeLogger } from ".";
 import { app } from "../../base";
-import { PopulateRef, UnpackRefType } from "../../utils";
+import { UnpackRefType } from "../../utils";
 import { initDB } from "../../utils/mongoose";
+
+/** DB Types */
+
+// Schedules
+const dbSchedulesProjection = { hor_app: 1, hor_estime: 1 } satisfies Partial<
+  Record<keyof dbTBM_Schedules_rt, 1>
+>;
+type dbScheduleRt = Pick<dbTBM_Schedules_rt, keyof typeof dbSchedulesProjection>;
+
+// Scheduled Routes
+const dbScheduledRoutesProjection = { _id: 1, stops: 1, trips: 1 } satisfies Partial<
+  Record<keyof dbTBM_ScheduledRoutes, 1>
+>;
+type dbScheduledRoute = Pick<dbTBM_ScheduledRoutes, keyof typeof dbScheduledRoutesProjection>;
+interface ScheduledRoutesOverwritten /* extends dbTBM_ScheduledRoutes */ {
+  stops: UnpackRefType<dbScheduledRoute["stops"]>;
+  trips: {
+    tripId: dbScheduledRoute["trips"][number]["tripId"];
+    // Not a Document because of lean
+    schedules: dbScheduleRt[];
+  }[];
+}
+type ScheduledRoute = Omit<dbScheduledRoute, keyof ScheduledRoutesOverwritten> & ScheduledRoutesOverwritten;
+
+// Stops
+const dbStopProjection = { _id: 1, coords: 1 };
+type Stop = Pick<dbTBM_Stops, keyof typeof dbStopProjection>;
+
+// Non Schedules Routes
+const dbNonScheduledRoutesProjection = { from: 1, to: 1, distance: 1 } satisfies Partial<
+  Record<keyof dbFootPaths, 1>
+>;
+type dbNonScheduledRoute = Pick<dbFootPaths, keyof typeof dbNonScheduledRoutesProjection>;
+interface NonScheduledRoutesOverwritten extends dbNonScheduledRoute {
+  from: UnpackRefType<dbNonScheduledRoute["from"]>;
+  to: UnpackRefType<dbNonScheduledRoute["to"]>;
+}
+type NonScheduledRoute = Omit<dbNonScheduledRoute, keyof NonScheduledRoutesOverwritten> &
+  NonScheduledRoutesOverwritten;
 
 if (parentPort) {
   const logger = new Logger(preComputeLogger, `[${(__filename.split(sep).pop() ?? "").split(".")[0]}]`);
@@ -29,47 +68,6 @@ if (parentPort) {
     TBMSchedulesInit(sourceDataDB);
     const TBMScheduledRoutesModel = TBMScheduledRoutesModelInit(sourceDataDB);
     const NonScheduledRoutesModel = NonScheduledRoutesModelInit(sourceDataDB);
-
-    /** DB Types */
-
-    type dbTBM_Schedules_rt = PopulateRef<dbScheduledRoute["trips"][number]["schedules"]>[number];
-    // Schedules
-    const dbSchedulesProjection = { hor_app: 1, hor_estime: 1 } satisfies Partial<
-      Record<keyof dbTBM_Schedules_rt, 1>
-    >;
-    type dbScheduleRt = Pick<dbTBM_Schedules_rt, keyof typeof dbSchedulesProjection>;
-
-    // Scheduled Routes
-    const dbScheduledRoutesProjection = { _id: 1, stops: 1, trips: 1 } satisfies Partial<
-      Record<keyof dbTBM_ScheduledRoutes, 1>
-    >;
-    type dbScheduledRoute = Pick<dbTBM_ScheduledRoutes, keyof typeof dbScheduledRoutesProjection>;
-    interface ScheduledRoutesOverwritten /* extends dbTBM_ScheduledRoutes */ {
-      stops: UnpackRefType<dbScheduledRoute["stops"]>;
-      trips: {
-        tripId: dbScheduledRoute["trips"][number]["tripId"];
-        // Not a Document because of lean
-        schedules: dbScheduleRt[];
-      }[];
-    }
-    type ScheduledRoute = Omit<dbScheduledRoute, keyof ScheduledRoutesOverwritten> &
-      ScheduledRoutesOverwritten;
-
-    // Stops
-    const dbStopProjection = { _id: 1, coords: 1 };
-    type Stop = Pick<dbTBM_Stops, keyof typeof dbStopProjection>;
-
-    // Non Schedules Routes
-    const dbNonScheduledRoutesProjection = { from: 1, to: 1, distance: 1 } satisfies Partial<
-      Record<keyof dbFootPaths, 1>
-    >;
-    type dbNonScheduledRoute = Pick<dbFootPaths, keyof typeof dbNonScheduledRoutesProjection>;
-    interface NonScheduledRoutesOverwritten extends dbNonScheduledRoute {
-      from: UnpackRefType<dbNonScheduledRoute["from"]>;
-      to: UnpackRefType<dbNonScheduledRoute["to"]>;
-    }
-    type NonScheduledRoute = Omit<dbNonScheduledRoute, keyof NonScheduledRoutesOverwritten> &
-      NonScheduledRoutesOverwritten;
 
     const dbScheduledRoutes = (await TBMScheduledRoutesModel.find<DocumentType<ScheduledRoute>>(
       {},
