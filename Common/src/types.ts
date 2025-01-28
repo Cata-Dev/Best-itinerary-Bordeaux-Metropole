fa6fec82becdbff902be36b8b0e45f94645cc013
Catatomik <cata@catadev.org>
2025-01-28 01:30:00 +0100

    ✨ Satisfy type

--- Common/src/types.ts
+++ Common/src/types.ts
@@ -1,3 +1,5 @@
 type KeyOfMap<M extends Map<unknown, unknown>> = M extends Map<infer K, unknown> ? K : never;
 
-export { KeyOfMap };
+type Satisfy<B, T extends B> = T;
+
+export { KeyOfMap, Satisfy };
