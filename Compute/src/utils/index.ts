05f78afb5f44de368c7b492cf19ceaca619b570e
Catatomik <cata@catadev.org>
2025-07-06 00:22:29 +0200

    ⚡ Factorize

--- Compute/src/utils/index.ts
+++ Compute/src/utils/index.ts
@@ -28,34 +28,6 @@ type UnionToTuple<T> =
     : [];
 
 /**
- * @description Search for a value in a **sorted** array, in O(log2(n)).
- * @param arr The **sorted** array where performing the search
- * @param el The element to look for, which will be compared
- * @param compare A compare function that takes 2 arguments : `a` el and `b` an element of the array.
- * It returns :
- *    - a negative number if `a` is before `b`;
- *    - 0 if `a` is equal to `b`;
- *    - a positive number of `a` is after `b`.
- * @returns The index of el if positive ; index of insertion if negative
- */
-function binarySearch<T, C>(arr: T[], el: C, compare: (a: C, b: T) => number) {
-  let low = 0;
-  let high = arr.length - 1;
-  while (low <= high) {
-    const mid = (high + low) >> 1; // x >> 1 == Math.floor(x/2)
-    const cmp = compare(el, arr[mid]);
-    if (cmp > 0) {
-      low = mid + 1;
-    } else if (cmp < 0) {
-      high = mid - 1;
-    } else {
-      return mid;
-    }
-  }
-  return ~low; // ~x == -x-1
-}
-
-/**
  * Initialize every property of {@link obj} defined in {@link defaults}.
  * @param obj Object to initialize its properties
  * @param defaults Default properties
@@ -68,5 +40,5 @@ function withDefaults<O extends object>(obj: Partial<O>, defaults: O): O {
   ) as O;
 }
 
-export { binarySearch, withDefaults };
+export { withDefaults };
 export type { PopulateRef, UnionToTuple, UnpackRefType };
--- Server/src/utils/index.ts
+++ Server/src/utils/index.ts
@@ -71,34 +71,6 @@ export function bulkUpsertAndPurge<
   ]);
 }
 
-/**
- * @description Search for a value in a **sorted** array, in O(log2(n)).
- * @param arr The **sorted** array where performing the search
- * @param el The element to look for, which will be compared
- * @param compare A compare function that takes 2 arguments : `a` el and `b` an element of the array.
- * It returns :
- *    - a negative number if `a` is before `b`;
- *    - 0 if `a` is equal to `b`;
- *    - a positive number of `a` is after `b`.
- * @returns The index of el if positive ; index of insertion if negative
- */
-export function binarySearch<T, C>(arr: T[], el: C, compare: (a: C, b: T) => number) {
-  let low = 0;
-  let high = arr.length - 1;
-  while (low <= high) {
-    const mid = (high + low) >> 1; // x >> 1 == Math.floor(x/2)
-    const cmp = compare(el, arr[mid]);
-    if (cmp > 0) {
-      low = mid + 1;
-    } else if (cmp < 0) {
-      high = mid - 1;
-    } else {
-      return mid;
-    }
-  }
-  return ~low; // ~x == -x-1
-}
-
 export const sumObj = <T extends number>(obj: Record<string, T> = {}, keys = []) => {
   return keys
     .map((k) => String(k))
