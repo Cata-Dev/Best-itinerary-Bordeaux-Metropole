198840a684d943f9e2d1546d1c236b110b269a76
Catatomik <cata@catadev.org>
2025-07-05 19:44:06 +0200

    🐛🔊 Harden address parsing

--- Server/src/externalAPIs/TBM/endpoints/addresses.endpoint.ts
+++ Server/src/externalAPIs/TBM/endpoints/addresses.endpoint.ts
@@ -4,6 +4,7 @@ import TBM_Addresses, { dbAddresses } from "@bibm/data/models/TBM/addresses.mode
 import { TBMEndpoints } from "@bibm/data/models/TBM/index";
 import { BaseTBM } from "..";
 import { Application } from "../../../declarations";
+import { logger } from "../../../logger";
 import { bulkOps } from "../../../utils";
 import { Endpoint } from "../../endpoint";
 
@@ -31,22 +32,31 @@ export default async (app: Application, getData: <T>(id: string, queries?: strin
       async () => {
         const rawAddresses: Addresse[] = await getData("fv_adresse_p", ["crs=epsg:2154"]);
 
-        const Addresses: dbAddresses[] = rawAddresses.map((address) => {
-          const voie = address.properties.nom_voie;
-          return {
-            _id: parseInt(address.properties.gid),
-            coords: address.geometry.coordinates,
-            numero: address.properties.numero,
-            rep: address.properties.rep?.toLowerCase(),
-            type_voie: voie?.match(/[A-zÀ-ÿ]+/g)?.[0] ?? "",
-            nom_voie: voie,
-            nom_voie_norm: normalize(voie),
-            code_postal:
-              parseInt(address.properties.cpostal) || parseInt(`33${address.properties.cinsee}`) || 0,
-            fantoir: address.properties.fantoir,
-            commune: address.properties.commune,
-          };
-        });
+        const Addresses: dbAddresses[] = rawAddresses
+          .map((address) => {
+            try {
+              const voie = address.properties.nom_voie;
+              return {
+                _id: parseInt(address.properties.gid),
+                coords: address.geometry.coordinates,
+                numero: address.properties.numero,
+                rep: address.properties.rep?.toLowerCase(),
+                type_voie: voie.match(/[A-zÀ-ÿ]+/g)?.[0] ?? "",
+                nom_voie: voie,
+                nom_voie_norm: normalize(voie),
+                code_postal:
+                  parseInt(address.properties.cpostal) || parseInt(`33${address.properties.cinsee}`) || 0,
+                fantoir: address.properties.fantoir,
+                commune: address.properties.commune,
+              };
+            } catch (err) {
+              if (app.get("debug"))
+                logger.debug(`Error while parsing address ${JSON.stringify(address)}`, err);
+
+              return null;
+            }
+          })
+          .filter((address) => !!address);
 
         const bulked = await Address.bulkWrite(
           bulkOps("updateOne", Addresses as unknown as Record<keyof dbAddresses, unknown>[]),
